722bb37d28186a3074751f06d8db9036b6390792
Wojciech Piech <ilikeahottodogu@gmail.com>
2025-06-30 00:07:01 +0200

    basic check

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div ref="container" class="w-screen h-screen" />
+</template>
+
+<script setup lang="ts">
+import Sigma from 'sigma'
+import Graph from 'graphology'
+
+const container = ref<HTMLDivElement | null>(null)
+
+onMounted(() => {
+  if (!container.value) return
+
+  const graph = new Graph()
+  graph.addNode('n1', { label: 'Node 1', x: 0, y: 0, size: 10, color: '#f66' })
+  graph.addNode('n2', { label: 'Node 2', x: 1, y: 1, size: 10, color: '#6f6' })
+  graph.addEdge('n1', 'n2')
+
+  new Sigma(graph, container.value)
+})
+</script>
