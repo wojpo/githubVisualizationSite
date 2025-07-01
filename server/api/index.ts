59605e4a1eec58098c14462f076c99fbc57590f7
Wojciech Piech <ilikeahottodogu@gmail.com>
2025-07-01 18:49:03 +0200

    index route

---
+++ server/api/index.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler(() => {
+  return {
+    name: 'github-visualization',
+    version: '1.0.0',
+    author: 'https://github.com/wojpo',
+    repository: 'https://github.com/wojpo/github-visualization',
+  }
+})
