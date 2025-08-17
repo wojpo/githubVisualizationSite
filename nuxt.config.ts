d3b49873663d39db350a3f78ebb41487e102120f
Wojciech Piech <129212595+wojpo@users.noreply.github.com>
2025-08-17 05:15:14 +0200

    resolve

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,5 +29,6 @@ export default defineNuxtConfig({
       GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     },
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    AUTH_SECRET: process.env.NUXT_AUTH_SECRET,
   },
 })
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,7 +4,7 @@ import GithubProvider from 'next-auth/providers/github'
 const runtimeConfig = useRuntimeConfig()
 
 export default NuxtAuthHandler({
-  secret: process.env.NUXT_AUTH_SECRET,
+  secret: runtimeConfig.AUTH_SECRET,
   providers: [ // @ts-expect-error Use .default here for it to work during SSR.
     GithubProvider.default({
       clientId: runtimeConfig.public.GITHUB_CLIENT_ID,
